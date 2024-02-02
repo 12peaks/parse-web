6c9e0d1592a0ccc8ae3d499a8132ef167a9b72f7
Tyler Myracle <tyler.myracle@gmail.com>
2024-02-01 18:15:40 -0600

    set request type in axios

--- libs/axios.ts
+++ libs/axios.ts
@@ -4,6 +4,7 @@ const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
   headers: {
     "Content-Type": "application/json",
+    Accept: "application/json",
   },
   withCredentials: true,
 });
