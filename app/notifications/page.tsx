383108d3a6edafc9494fd68a0711e91fa208ec7e
Tyler Myracle <tyler.myracle@gmail.com>
2024-02-29 13:02:40 -0600

    notif page error handling

--- app/notifications/page.tsx
+++ app/notifications/page.tsx
@@ -1,14 +1,28 @@
 "use client";
+import { showNotification } from "@mantine/notifications";
 import { useQuery } from "@tanstack/react-query";
 import { getNotifications } from "@/api/notifications";
 import { NotificationPageHeader } from "@/app/_components/notifications/NotificationPageHeader";
 import { NotificationTable } from "@/app/_components/notifications/NotificationTable";
 
 export default function Notifications() {
-  const { data: notifications, isLoading } = useQuery({
+  const {
+    data: notifications,
+    isLoading,
+    isError,
+    error,
+  } = useQuery({
     queryKey: ["notifications"],
     queryFn: getNotifications,
   });
+
+  if (isError) {
+    showNotification({
+      title: "Error",
+      message: error.message,
+      color: "red",
+    });
+  }
 
   return (
     <>
