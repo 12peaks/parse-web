f38275203a444ac1f2a85151f0e49ce67e8e1a9e
Tyler Myracle <tyler.myracle@gmail.com>
2024-03-01 11:21:50 -0600

    error handling for goals page

--- app/goals/page.tsx
+++ app/goals/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useModals } from "@mantine/modals";
-import { Button, Loader } from "@mantine/core";
+import { Loader } from "@mantine/core";
+import { showNotification } from "@mantine/notifications";
 import { useQuery } from "@tanstack/react-query";
 import { getGoals } from "@/api/goals";
 import { GoalModal } from "@/app/_components/goals/GoalModal";
@@ -10,7 +11,12 @@ import { GoalsTable } from "@/app/_components/goals/GoalsTable";
 export default function Goals() {
   const modals = useModals();
 
-  const goalsQuery = useQuery({
+  const {
+    data: goals,
+    isLoading,
+    isError,
+    error,
+  } = useQuery({
     queryKey: ["goals"],
     queryFn: () => getGoals(),
   });
@@ -23,24 +29,27 @@ export default function Goals() {
     });
   };
 
-  if (goalsQuery.isLoading) {
+  if (isError) {
+    showNotification({
+      title: "Error",
+      message: error.message,
+      color: "red",
+    });
+  }
+
+  if (isLoading) {
     return <Loader size="xl" className="mx-auto mt-24" />;
   }
 
   return (
     <>
-      {goalsQuery.data && goalsQuery.data.length === 0 ? (
+      {goals && goals.length === 0 ? (
         <GoalsEmptyState handleGoalCreate={openNewGoalModal} />
       ) : (
         <div>
-          {goalsQuery.data &&
-            goalsQuery.isSuccess &&
-            goalsQuery.data.length > 0 && (
-              <GoalsTable
-                goals={goalsQuery.data}
-                handleGoalCreate={openNewGoalModal}
-              />
-            )}
+          {goals && goals.length > 0 && (
+            <GoalsTable goals={goals} handleGoalCreate={openNewGoalModal} />
+          )}
         </div>
       )}
     </>
